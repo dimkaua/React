fcbb584d1eadd29790bb6bcba4673f505afa30c8
Dima Gorobets <dimkaua91@gmail.com>
2022-09-16 15:16:33 +0300

    sersor_store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,202 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          {' '}
+          <img width={60} height={60} src="/img/logo.png" />
+          <div>
+            <h3>Sensor Shop</h3>
+            <p>Великий вибір сенсорів</p>
+          </div>
+        </div>
+        <ul className="headerRight">
+          <li>
+            <img width={30} height={30} src="/img/basket.png" />
+            <span>1800 грн.</span>
+          </li>
+          <li>
+            <img width={30} height={30} src="/img/person.png" />
+          </li>
+        </ul>
       </header>
+      <div className="content">
+        <h1>Всі сенсори</h1>
+
+        <div className="sensor_card">
+          <div className="card">
+            <img
+              width={150}
+              height={150}
+              src="/img/sensor/1.jpg"
+              alt="Sensor"
+            />
+            <h5>
+              Бездротовий датчик детектування диму та чадного газу Ajax
+              FireProtect Plus EU Black
+            </h5>
+            <div className="cardBottom">
+              <div>
+                <span>Ціна:</span>
+                <b>2 960 грн.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.png" alt="Plus" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={100}
+              height={150}
+              src="/img/sensor/2.jpg"
+              alt="Sensor"
+            />
+            <h5>
+              Бездротовий датчик руху універсальний Ajax MotionProtect White
+              ID1254563211
+            </h5>
+            <div className="cardBottom">
+              <div>
+                <span>Ціна:</span>
+                <b>1 499 грн.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.png" alt="Plus" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={100}
+              height={150}
+              src="/img/sensor/3.jpg"
+              alt="Sensor"
+            />
+            <h5>
+              Датчик відчинення дверей/вікна із сенсором удару та нахилу Ajax
+              DoorProtect Plus White
+            </h5>
+            <div className="cardBottom">
+              <div>
+                <span>Ціна:</span>
+                <b>1 500 грн.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.png" alt="Plus" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={150}
+              height={150}
+              src="/img/sensor/4.jpg"
+              alt="Sensor"
+            />
+            <h5>Бездротовий датчик диму Ajax FireProtect White ID848487611</h5>
+            <div className="cardBottom">
+              <div>
+                <span>Ціна:</span>
+                <b>2 000 грн.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.png" alt="Plus" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img width={90} height={150} src="/img/sensor/5.jpg" alt="Sensor" />
+            <h5>
+              Бездротовий датчик руху з фотофіксацією Ajax MotionCam Білий
+            </h5>
+            <div className="cardBottom">
+              <div>
+                <span>Ціна:</span>
+                <b>3 800 грн.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.png" alt="Plus" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={100}
+              height={150}
+              src="/img/sensor/6.jpg"
+              alt="Sensor"
+            />
+            <h5>
+              Бездротовий датчик відкриття дверей/вікна Ajax DoorProtect Black
+            </h5>
+            <div className="cardBottom">
+              <div>
+                <span>Ціна:</span>
+                <b>999 грн.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.png" alt="Plus" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img width={90} height={150} src="/img/sensor/7.jpg" alt="Sensor" />
+            <h5>
+              Бездротовий вуличний датчик руху Ajax MotionProtect Outdoor White
+            </h5>
+            <div className="cardBottom">
+              <div>
+                <span>Ціна:</span>
+                <b>4 600 грн.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.png" alt="Plus" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={100}
+              height={150}
+              src="/img/sensor/8.jpg"
+              alt="Sensor"
+            />
+            <h5>Бездротовий датчик руху Ajax MotionProtect Plus EU Black</h5>
+            <div className="cardBottom">
+              <div>
+                <span>Ціна:</span>
+                <b>2 300 грн.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.png" alt="Plus" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={100}
+              height={150}
+              src="/img/sensor/9.jpg"
+              alt="Sensor"
+            />
+            <h5>
+              Бездротовий датчик руху штора Ajax MotionProtect Curtain Чорний
+            </h5>
+            <div className="cardBottom">
+              <div>
+                <span>Ціна:</span>
+                <b>1 900 грн.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.png" alt="Plus" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
